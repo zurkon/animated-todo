ae4fdff24c22ec58c62cc74514f816fa6faeab52
zurkon <46505446+zurkon@users.noreply.github.com>
2022-03-18 19:21:03 +0000

    Deploying to gh-pages from @ zurkon/animated-todo@24faec30d48849a72c436f33e68bfef699ec5338 🚀

---
+++ _next/static/Lqj4vxVxvLNMCqBbZdyXy/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/890-f4173563d285a4ca.js","static/chunks/pages/index-e24363bc042ec03e.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
